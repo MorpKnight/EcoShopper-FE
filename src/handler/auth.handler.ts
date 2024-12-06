64f7b847c115851f9a5173980804e64444da4b7b
MorpKnight <christoffelsihombing@gmail.com>
2024-12-06 07:00:35 +0700

    Add admin login functionality and product fetching logic

--- src/handler/auth.handler.ts
+++ src/handler/auth.handler.ts
@@ -41,3 +41,19 @@ export const loginEmail = async (email: string, password: string) => {
   localStorage.setItem('token', response.data.token);
   return { message: response.data.message };
 };
+
+export const loginEmailAdmin = async (email: string, password: string) => {
+  if (!email.match(/^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/))
+    throw new Error('Invalid email address');
+  if (password.length < 8)
+    throw new Error('Password must be at least 8 characters long');
+  const response = await axios.post(`${BE_URI}/auth/login-admin`, {
+    email,
+    password,
+  });
+
+  if (response.status !== 200) throw new Error(response.data.error);
+
+  localStorage.setItem('token', response.data.token);
+  return { message: response.data.message };
+}
